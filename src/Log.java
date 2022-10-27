c0c3936ea9e69f0e08b94f9429d7b2b6a31e6238
Jesus Betancourt <jbeta02@gmail.com>
2022-10-27 14:32:33 -0700

    Add Log class and change Main to show usage

---
+++ src/Log.java
@@ -0,0 +1,54 @@
+/*
+ * Name:        Jesus Betancourt
+ *
+ * Course:      CS-12, Fall 2022
+ *
+ * Date:        10/27/22
+ *
+ * Filename:    Log.java
+ *
+ * Purpose:     To act as a wrapper to System.out.println() and to provide testing utilities
+ */
+
+public class Log {
+
+    // print headers
+    private static final String LOG = "[ LOG ] ";
+    private static final String ERROR = "<ERROR> ";
+    private static final String EXCEPTION_START = " | Error msg";
+
+    // print format
+    private static final String FORMAT = "%-50s %s\n";
+
+    // general print functions for logging
+
+    // base print function
+    private static void print(String logType, String message, Object value){
+        System.out.printf(FORMAT, logType + message + ":", value);
+    }
+
+    // overloaded print, add message to log, print message and value
+    public static void print(String message, Object value){
+        print(LOG, message, value);
+    }
+
+    // overloaded print, make message empty, print value
+    public static void print(Object value){
+        print("", value);
+    }
+
+    // print functions for logging errors
+
+//    public static void  error(String message, Object value){
+//        print(ERROR, message, value);
+//    }
+
+    public static void error(String message, String exceptionMessage){
+        print(ERROR, message + EXCEPTION_START, exceptionMessage);
+    }
+
+    // overloaded error, make message empty, print value
+    public static void error(Object value){
+        print(ERROR, "", value);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
-import com.mpatric.mp3agic.Mp3File;
+/*
+ * Name:        Jesus Betancourt
+ *
+ * Course:      CS-12, Fall 2022
+ *
+ * Date:        10/27/22
+ *
+ * Filename:    Main.java
+ *
+ * Purpose:     Main program class
+ */
+
 import org.jaudiotagger.audio.AudioFileIO;
-import org.jaudiotagger.audio.AudioHeader;
 import org.jaudiotagger.audio.mp3.MP3File;
 import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
@@ -9,32 +19,6 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-
-        // mp3agic example
-//        Mp3File mp3File1;
-//
-//        try {
-//            mp3File1 = new Mp3File("test-mp3/She's Back.mp3");
-//
-//            if (mp3File1.hasId3v1Tag()){
-//                System.out.println("has 3v1 tag");
-//            }
-//
-//            if (mp3File1.hasId3v2Tag()){
-//                System.out.println("has 3v2 tag"); // the mp3 files I have use ID3v2 Tags
-//            }
-//        }
-//        catch (Exception e){
-//            System.out.println("Error: " + e.getMessage());
-//        }
-
-        /////////////////////////////////////////////////////////////////////
-
-        // jaudiotagger example
-
-        //TODO I like jaudiotagger better since it has more features and supports more formats
-        // continue the project using this library instead of the other one
-
         MP3File mp3File2;
 
         File file = new File("test-mp3/DOA.mp3");
@@ -43,11 +27,11 @@ public class Main {
             mp3File2 = (MP3File) AudioFileIO.read(file);
 
             if (mp3File2.hasID3v1Tag()){
-                System.out.println("has 3v1 tag");
+                Log.print("has 3v1 tag");
             }
 
             if (mp3File2.hasID3v2Tag()){
-                System.out.println("has 3v2 tag"); // the mp3 files I have use ID3v2 Tags
+                Log.print("has 3v2 tag"); // the mp3 files I have use ID3v2 Tags
             }
 
             Tag tag = mp3File2.getTag();
@@ -57,7 +41,14 @@ public class Main {
             System.out.println(s);
         }
         catch (Exception e){
-            System.out.println("Error: " + e.getMessage());
+            Log.error("" + e.getMessage());
         }
+
+        Log.print("input");
+        Log.print("This is mp3 file 2", "input");
+        Log.print("mp3 file 3", "input");
+
+        Log.error("didn't find tags", "input");
+        Log.error("cant read mp3", "input");
     }
 }
