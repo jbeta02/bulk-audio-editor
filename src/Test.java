a09804c73a32433b0eb808b7f1c346887aa8c763
Jesus Betancourt <jbeta02@gmail.com>
2022-11-04 15:52:15 -0700

    renamed Main to Test to reflect its purpose better

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 // Purpose: Quick testing class
 
-public class Main {
+public class Test {
     public static void main(String[] args) {
 
 //        MP3Editor mp3Editor = new MP3Editor("test-mp3/2Volt.mp3");
