bc8d436e2f78a9c38d9040f04462aa59913fef8e
Jesus Betancourt <jbeta02@gmail.com>
2022-11-08 23:09:52 -0800

    remove test mp3 file

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@
 public class Test {
     public static void main(String[] args) {
 
-        MP3Editor mp3Editor = new MP3Editor("test-mp3/Volt.mp3");
+        MP3Editor mp3Editor = new MP3Editor("test-mp3/2Volt.mp3");
         //MP3Editor mp3Editor = new MP3Editor("test-mp3/");
 
         //mp3Editor.removeFromFileName("2");
