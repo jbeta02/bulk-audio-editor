18bd7bbd1a1c9bb91b073143f376540c605eafc6
Jesus Betancourt <jbeta02@gmail.com>
2022-11-24 17:30:27 -0800

    add gettters and setters

--- src/MP3Editor.java
+++ src/MP3Editor.java
@@ -1,5 +1,4 @@
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,73 +8,25 @@ import java.util.ArrayList;
 // jaudiotagger is an external library for manipulating mp3 files
 import org.jaudiotagger.audio.AudioFileIO;
 import org.jaudiotagger.audio.mp3.MP3File;
-import org.jaudiotagger.tag.FieldDataInvalidException;
 import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
 import org.jaudiotagger.tag.images.Artwork;
 import org.jaudiotagger.tag.images.ArtworkFactory;
-import org.jaudiotagger.tag.images.StandardArtwork;
 
 // Purpose: Provide means to modify an MP3 file. Modifications include name, metadata, and volume.
+
+//TODO improve error for input related and unrelated errors handling and reporting
 
 public class MP3Editor {
 
     private ArrayList<MP3File> mp3Files = new ArrayList<>();
+    private String path = "";
 
     // Constructor used to populate mp3Files ArrayList so that it may be used in other methods after instantiation
     public MP3Editor(String path){
-        File file = new File(path);
-
-        // check if path leads to folder
-        if (file.isDirectory() & !file.isFile()){
-            Log.print("is folder");
-
-            // loop through files, convert each to MP3 obj and add to arraylist
-            for(File fileInLoop : file.listFiles()){
-                Log.print("file", fileInLoop);
-
-                // TODO determine if file found is mp3
-
-                // might not be able to read file
-                try {
-                    MP3File convertedFile = (MP3File) AudioFileIO.read(fileInLoop);
-
-                    mp3Files.add(convertedFile);
-                }
-                catch (Exception e){
-                    Log.error("Unable to internally classify file with name", fileInLoop.getName());
-                    Log.error("Error msg", e.getMessage());
-                }
-            }
-
-            // verify number of files found
-            Log.print("number of files found", mp3Files.size());
-        }
-
-        // check if path leads to file
-        else if (!file.isDirectory() & file.isFile()){
-            Log.print("is file");
-
-            // might not be able to read file
-            try {
-                MP3File convertedFile = (MP3File) AudioFileIO.read(file);
-
-                mp3Files.add(convertedFile);
-            }
-            catch (Exception e){
-                Log.error("Unable to internally classify file with name", file.getName());
-                Log.error("Error msg", e.getMessage());
-            }
-
-            // verify that there is only one file
-            Log.print("Only one file found", mp3Files.size() == 1);
-        }
-
-        // path resulted in neither folder nor file, or led to both folder and file
-        else {
-            Log.error("Unable to determine if path leads to folder or file");
-            Log.error("isFolder, isFile", file.isDirectory() +  " , " + file.isFile());
-        }
+        // set instance variables
+        setPath(path);
+        setFiles(path);
     }
 
     // add text to either beginning (true) or end (false) of file
@@ -201,10 +152,9 @@ public class MP3Editor {
                 // save new cover art
                 file.commit();
 
-                //TODO do more testing on created files. Why can't I see them (trying checking for them with cmd)
-
                 // will create an "AlbumArtSmall.jpg"
                 // will create a "Folder.jpg"
+                // seem to only be visible in IDE
 
                 // files seem to be created when Windows Media Player is opened. Files can be deleted after they are created
                 // without any apparent consequence
@@ -215,16 +165,105 @@ public class MP3Editor {
         }
     }
 
+    public void displayData(){
+        //TODO cont here
+
+        // get all songs then print list of songs and data
+
+        // folder name      total files
+
+        // name     artist      album   genre       len
+
+        System.out.printf("%s %s", getPath(), );
+
+    }
+
     // return file's path but exclude the file from the path string
     private String getPathNoName(File file){
         // get current file path
         String currentPath = file.getPath();
 
         // remove current file name from path
-        // create new string starting from beginning of path and ending at \ (inclusive)
         // (will remove file name and extension)
+        // create new string starting from beginning of path and ending at \ (inclusive)
         String newPath = currentPath.substring(0, currentPath.lastIndexOf("\\") + 1);
 
         return newPath;
+    }
+
+    // accessors and mutators ----------------------------------------------
+
+    public ArrayList<MP3File> getFiles(){
+        return mp3Files;
+    }
+
+    public void setFiles(String path) {
+        File file = new File(path);
+
+        // check if path leads to folder
+        if (file.isDirectory() & !file.isFile()) {
+            Log.print("is folder");
+
+            // loop through files, convert each to MP3 obj and add to arraylist
+            for (File fileInLoop : file.listFiles()) {
+                Log.print("file", fileInLoop);
+
+                // TODO determine if file found is mp3
+
+                // might not be able to read file
+                try {
+                    MP3File convertedFile = (MP3File) AudioFileIO.read(fileInLoop);
+
+                    mp3Files.add(convertedFile);
+                } catch (Exception e) {
+                    Log.error("Unable to internally classify file with name", fileInLoop.getName());
+                    Log.error("Error msg", e.getMessage());
+                }
+            }
+
+            // verify number of files found
+            Log.print("number of files found", mp3Files.size());
+        }
+
+        // check if path leads to file
+        else if (!file.isDirectory() & file.isFile()) {
+            Log.print("is file");
+
+            // might not be able to read file
+            try {
+                MP3File convertedFile = (MP3File) AudioFileIO.read(file);
+
+                mp3Files.add(convertedFile);
+            } catch (Exception e) {
+                Log.error("Unable to internally classify file with name", file.getName());
+                Log.error("Error msg", e.getMessage());
+            }
+
+            // verify that there is only one file
+            Log.print("Only one file found", mp3Files.size() == 1);
+        }
+    }
+
+    public String getPath(){
+        return path;
+    }
+
+    public boolean setPath(String path){
+        // check if path is valid
+        boolean pathSet = false;
+
+        // create path obj so the path can be validated
+        File file = new File(path);
+
+        // set path if valid
+        if (file.isDirectory() || file.isFile()){
+            this.path = path;
+            pathSet = true;
+        }
+        else {
+            Log.error("Path not found.");
+        }
+
+        return pathSet;
     }
 }
