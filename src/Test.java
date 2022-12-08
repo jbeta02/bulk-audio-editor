56c8136fcd42d0416f0ebaffab1569738fdb10f2
Jesus Betancourt <jbeta02@gmail.com>
2022-12-07 22:10:24 -0800

    current test

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@
 public class Test {
     public static void main(String[] args) {
 
-        MP3Editor mp3Editor = new MP3Editor("test-mp3/"); // test-mp3/
+        MP3Editor mp3Editor = new MP3Editor("test-mp3/out23-This Fffire.mp3"); // test-mp3/
         //MP3Editor mp3Editor = new MP3Editor("test-mp3/");
 
         //mp3Editor.removeFromFileName("2");
@@ -16,9 +16,20 @@ public class Test {
 //        String inputFile = "\"F:\\Software\\Java Apps\\MP3Editor\\test-mp3\\in-She's Back.mp3\"";
 //        String outputFile = "\"F:\\Software\\Java Apps\\MP3Editor\\test-mp3\\out23-She's Back.mp3\"";
 //
-        String inputFile = "\"F:\\Software\\Java Apps\\MP3Editor\\test-mp3\\in-This Fffire.mp3\""; // test-mp3\copy-out-This Fffire.mp3
-        String outputFile = "\"F:\\Software\\Java Apps\\MP3Editor\\test-mp3\\out23-This Fffire.mp3\"";
+        // add \" for ffmpgeg direct inputs and no \" when entering as path such as for FileHandler stuff
+        String inputFile = "F:\\Software\\Java Apps\\MP3Editor\\test-mp3\\"; // F:\Software\Java Apps\MP3Editor\test-mp3\out23-This Fffire.mp3
+        String outputFile = "F:\\Software\\Java Apps\\MP3Editor\\test-mp3\\out23-This Fffire.mp3";
+        String outputPath = "F:\\Software\\Java Apps\\MP3Editor\\test-mp3-2\\"; // F:\Software\Java Apps\MP3Editor\test-mp3-2
 
         //mp3Editor.normalizeLoudness(inputFile, -23, -2, outputFile); // default: -23, -2, 7 or preferred?: -16, -2, 7
+
+        // test: pass
+        Log.print("files before", mp3Editor.getFiles().size());
+
+        mp3Editor.skipOverrideDenials(outputPath);
+
+        //mp3Editor.moveFilesToOutput(outputPath);
+
+        Log.print("files after", mp3Editor.getFiles().size());
     }
 }
