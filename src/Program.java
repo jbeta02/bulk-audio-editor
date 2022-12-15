19b17a5130edc8ce8b4e397d6e055911a66e50f3
Jesus Betancourt <jbeta02@gmail.com>
2022-12-14 19:14:48 -0800

    remove dash from commands

--- src/Program.java
+++ src/Program.java
@@ -19,7 +19,7 @@ public class Program {
         System.out.println("Welcome to \"Bulk Audio Editor\"");
         System.out.println("-------------------------------");
         System.out.println("Enter path to a single Mp3 file or a folder holding MP3 files");
-        System.out.println("(enter \"help\" for more information)");
+        System.out.println("(enter h for more information)");
 
         // prompt for valid path
         path = promptForPath();
@@ -29,8 +29,6 @@ public class Program {
 
         // prompt for command
         command = promptForCommand(mp3Editor.getInputPath()); // command = array [command, commandInput, output path]
-
-        //TODO trim output path
 
         // check for valid output path then check if there are file conflicts
         if (!command[2].equals("")) {
@@ -47,10 +45,10 @@ public class Program {
             // if output valid then check file conflicts
             else {
 
-                // -a command handles name conflicts internally
-                if (!command[0].equals("-a")){
+                // cffAr and cffA commands handle name conflicts internally
+                if (!command[0].equals("cffAr") && !command[0].equals("cffA")){
                     // check if there are name conflicts
-                    // (if conflicts exist ask if overriding ok, if override not ok then skip file)
+                    // (if conflicts exists then ask if overriding ok, if override not ok then skip file)
                     mp3Editor.skipOverrideDenials(command[2]);
                 }
             }
@@ -58,85 +56,84 @@ public class Program {
 
         while(!command[0].equals("-q") & !command[0].equals("q")) {
             switch(command[0]) {
-                case "-ab":
+                case "ab":
                     mp3Editor.addToFileName(true, command[1], command[2]);
                     break;
 
-                case "-ae":
+                case "ae":
                     mp3Editor.addToFileName(false, command[1], command[2]);
                     break;
 
-                case "-r":
+                case "r":
                     mp3Editor.removeFromFileName(command[1], command[2]);
                     break;
 
-                case "-Ar":
+                case "Ar":
                     mp3Editor.modifyArtist(command[1], command[2]);
                     break;
 
-                case "-A":
+                case "A":
                     mp3Editor.modifyAlbum(command[1], command[2]);
                     break;
 
-                case "-G":
+                case "G":
                     mp3Editor.modifyGenre(command[1], command[2]);
                     break;
 
-                case "-Art":
+                case "Art":
                     mp3Editor.changeArt(command[1], command[2]);
                     break;
 
                 // normalize using default value of -16 LU
-                case "-LN":
+                case "LN":
                     mp3Editor.normalizeFiles(command[2]);
                     break;
 
                 // normalize with custom loudness
-                case "-LNN":
+                case "LNN":
                     mp3Editor.normalizeFiles(toDouble(command[1]), command[2]);
                     break;
 
-                case "-cffAr":
+                case "cffAr":
                     mp3Editor.createFoldersByArtist(command[2]);
                     break;
 
-                case "-cffA":
+                case "cffA":
                     mp3Editor.createFoldersByAlbum(command[2]);
                     break;
 
-                case "-DN":
+                case "DN":
                     mp3Editor.displayDataByName();
                     break;
 
-                case "-DAr":
+                case "DAr":
                     mp3Editor.displayDataByArtist();
                     break;
 
-                case "-DA":
+                case "DA":
                     mp3Editor.displayDataByAlbum();
                     break;
 
-                case "-DG":
+                case "DG":
                     mp3Editor.displayDataByGenre();
                     break;
 
-                case "-DL":
+                case "DL":
                     mp3Editor.displayDataByLoudness();
                     break;
 
-                case "-n":
+                case "n":
                     mp3Editor.setInputPath(command[1]);
                     mp3Editor.setFiles(command[1]);
                     break;
 
-                case "-h":
+                case "h":
                     displayHelp();
                     break;
 
                 default:
-                    System.out.println("\nPlease enter a valid command. Below is a list of all commands.");
-                    System.out.println("(List of commands can be produced again using \"-h\" command)\n");
-                    displayHelp();
+                    System.out.println("\nPlease enter a valid command.");
+                    System.out.println("(List of commands can be produced using \"h\" command)\n");
                     break;
             }
             command = promptForCommand(mp3Editor.getInputPath());
@@ -147,13 +144,14 @@ public class Program {
 
     // display a description of Bulk Audio Editor
     private static void displayAbout() {
-        System.out.print("\n\"Bulk Audio Editor\" is a tool to help edit audio files in bulk. \n" +
-                "A user can enter a path to a folder holding audio files such as mp3 then can run commands that can edit metadata such as: \n" +
+        System.out.print("\n\"Bulk Audio Editor\" is a tool to help edit and manage audio files in bulk. \n" +
+                "A user can enter a path to a folder holding audio files such as mp3 then run commands that can edit metadata such as: \n" +
                 "- Album\n" +
                 "- Artist\n" +
                 "- Genre\n" +
                 "- Cover Art\n" +
-                "and more\n" +
+                "The tool has other utilities such as loudness normalization, folder organization, and many more.\n" +
+                "----------\n" +
                 "After entering a path use the -h command to display all available commands.\n\n");
     }
 
@@ -161,47 +159,46 @@ public class Program {
     private static void displayHelp() {
         String format = "%-30s %s\n";
 
-        //TODO consider removing "-" from all commands so it is one less character to type
         System.out.printf(format, "-o [output path]: ", "Output modifier can be added to all commands to specify where to save output file after command");
         System.out.printf(format, "", "\t\tWill be either folder or file path depending on initial path set");
-        System.out.printf(format, "-ab [text]: ", "Add command will add [text] to beginning");
-        System.out.printf(format, "-ae [text]: ", "Add command will add [text] to end");
-        System.out.printf(format, "-r [text]: ", "Remove command will remove [text] from files");
-        System.out.printf(format, "-Ar [artist text]: ", "Artist command will add [artist text] to artist metadata of files");
-        System.out.printf(format, "-A [album text]: ", "Album command will add [album text] to album metadata of files");
-        System.out.printf(format, "-G [genre text]: ", "Genre command will add [genre text] to genre metadata of files");
-        System.out.printf(format, "-Art [path to art]: ", "Art command will add art in [path to art] to art metadata of files");
-        System.out.printf(format, "-LN: ", "Loudness Normalize command will make loudness of files similar.");
-        System.out.printf(format, "", "\t\tThis will allow user to listen to music without needing to change the volume. Will set loudness to -16 LUFS.");
+        System.out.printf(format, "ab [text]: ", "Add command will add [text] to beginning");
+        System.out.printf(format, "ae [text]: ", "Add command will add [text] to end");
+        System.out.printf(format, "r [text]: ", "Remove command will remove [text] from files");
+        System.out.printf(format, "Ar [artist text]: ", "Artist command will add [artist text] to artist metadata of files");
+        System.out.printf(format, "A [album text]: ", "Album command will add [album text] to album metadata of files");
+        System.out.printf(format, "G [genre text]: ", "Genre command will add [genre text] to genre metadata of files");
+        System.out.printf(format, "Art [path to art]: ", "Art command will add art in [path to art] to art metadata of files");
+        System.out.printf(format, "LN: ", "Loudness Normalize command will make loudness of files similar.");
+        System.out.printf(format, "", "\t\tThis will allow user to listen to music without needing to change the volume. Will set loudness to -16 LU.");
         System.out.printf(format, "", "\t\tInternally, true peak set to -2 and loudness range set to match file's current range");
-        System.out.printf(format, "-LNN [LUFS value]: ", "Loudness Normalize command (same as -LN but with custom loudness) will take a value in LUFS and bring the loudness of files to that target.");
-        System.out.printf(format, "", "\t\tThis will allow user to listen to music without needing to change the volume. Recommended LUFS values are -24 to -14 (numbers closer to 0 are louder).");
+        System.out.printf(format, "LNN [LUFS value]: ", "Loudness Normalize command (same as -LN but with custom loudness) will take a value in LU and bring the loudness of files to that target.");
+        System.out.printf(format, "", "\t\tThis will allow user to listen to music without needing to change the volume. Recommended LU values are -24 to -14 (numbers closer to 0 are louder).");
         System.out.printf(format, "", "\t\tInternally, true peak set to -2 and loudness range set to match file's current range\"");
-        System.out.printf(format, "-cffA: ", "Create folders for album will create folder for files based on their album then put the files in those folders");
-        System.out.printf(format, "-cffAr: ", "Create folders for artist will create folder for files based on their artist then put the files in those folders");
-        System.out.printf(format, "-DN: ", "Display by name command will display the metadata of the files organized by name");
-        System.out.printf(format, "-DAr: ", "Display by artist command will display the metadata of the files organized by artist name");
-        System.out.printf(format, "-DA: ", "Display by album command will display the metadata of the files organized by album");
-        System.out.printf(format, "-DG: ", "Display by genre command will display the metadata of the files organized by genre");
-        System.out.printf(format, "-DL: ", "Display by loudness command will display the metadata and loudness data of the files organized by loudest to quietest in LU");
-        System.out.printf(format, "-n [path to files]: ", "New command will select new set of files or file to target");
-        System.out.printf(format, "-h or help: ", "Help command will display all command options and give their descriptions");
-        System.out.printf(format, "-q or q: ", "Quit command will terminate program");
+        System.out.printf(format, "cffA: ", "Create folders for album will create folder for files based on their album then put the files in those folders");
+        System.out.printf(format, "cffAr: ", "Create folders for artist will create folder for files based on their artist then put the files in those folders");
+        System.out.printf(format, "DN: ", "Display by name command will display the metadata of the files organized by name");
+        System.out.printf(format, "DAr: ", "Display by artist command will display the metadata of the files organized by artist name");
+        System.out.printf(format, "DA: ", "Display by album command will display the metadata of the files organized by album");
+        System.out.printf(format, "DG: ", "Display by genre command will display the metadata of the files organized by genre");
+        System.out.printf(format, "DL: ", "Display by loudness command will display the metadata and loudness data of the files organized by loudest to quietest in LU");
+        System.out.printf(format, "n [path to files]: ", "New command will select new set of files or file to target");
+        System.out.printf(format, "h or help: ", "Help command will display all command options and give their descriptions");
+        System.out.printf(format, "q or q: ", "Quit command will terminate program");
         System.out.println();
 
         System.out.println("Examples------------------------------------");
         System.out.println("Add command with and without output path modifier");
-        System.out.println("-ab new-");
-        System.out.println("-ab new- -o C:folder1/folder2/");
+        System.out.println("ab new-");
+        System.out.println("ab new- -o C:folder1/folder2/");
         System.out.println("original: file.mp3 >>> after command: new-file.mp3");
         System.out.println();
 
         System.out.println("Loudness Normalization command with and without output path modifier");
-        System.out.println("-LN");
-        System.out.println("-LN -o C:folder1/folder2/");
+        System.out.println("LN");
+        System.out.println("LN -o C:folder1/folder2/");
         System.out.println("Loudness Normalization command with custom loudness with and without output path modifier");
-        System.out.println("-LNN -23");
-        System.out.println("-LNN -o C:folder1/folder2/out-file.mp3");
+        System.out.println("LNN -23");
+        System.out.println("LNN -o C:folder1/folder2/out-file.mp3");
     }
 
     // recursively prompt for path until a valid path is entered
@@ -222,7 +219,7 @@ public class Program {
         else {
             Log.error("Path not found. Please enter a valid path. Ex: C:\\folder1\\folder2\\");
 
-            if (path.equals("help")) {
+            if (path.equals("h") || path.equals("help")) {
                 displayAbout();
             }
             return promptForPath();
